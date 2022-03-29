126496e74c3b726a15d51cb6bf3b1c1265f83838
krzysztof sauermann <krzysztof.sauermann@outlook.com>
2022-03-29 22:47:41 +0200

    Test for Metamorphosis

--- src/app/components/sites/gallery-site/gallery-site.component.ts
+++ src/app/components/sites/gallery-site/gallery-site.component.ts
@@ -10,13 +10,18 @@ export class GallerySiteComponent implements AfterViewInit {
 
   popupShownGallerySalon = false;
   popupShownGalleryBeforeAfter = false;
+  popupShownMetamorphosis = false;
+  popupShownGalleryMetamorphosis = false;
+
   currentElement: number = 0;
   salonElements: GalleryElement[] = [];
   beforeAfterElements: GalleryElement[] = [];
-
+  metamorphosisElements: GalleryElement[] = [];
+  
   constructor() {
     this.initSalon();
     this.initBeforeAfter();
+    this.initMetamorphosis();
   }
   ngAfterViewInit(): void {
 
@@ -25,13 +30,23 @@ export class GallerySiteComponent implements AfterViewInit {
   ngOnInit(): void {
 
   }
-  showGallerySalon(i: number) {
-    this.currentElement = i;
+  showGallerySalon(i: number | null) {
+    if (i) {
+      this.currentElement = i;
+    }
     this.popupShownGallerySalon = true;
   }
-  showGalleryBeforeAfter(i: number) {
-    this.currentElement = i;
+  showGalleryBeforeAfter(i: number | null) {
+    if (i) {
+      this.currentElement = i;
+    }    
     this.popupShownGalleryBeforeAfter = true;
+  }
+  showGalleryMetamorphosis(i: number | null) {
+    if (i) {
+      this.currentElement = i;
+    }    
+    this.popupShownGalleryMetamorphosis = true;
   }
   initSalon() {
     for (let i = 1; i <= 6; i++) {
@@ -51,8 +66,27 @@ export class GallerySiteComponent implements AfterViewInit {
     this.loadBeforeAfter(photos);
     photos = [112, 113];
     this.loadBeforeAfter(photos);
-  }
+  } 
   private loadBeforeAfter(photos: number[]) {
     this.beforeAfterElements.push(new GalleryElement(`Przed i po`, [`assets/images/${photos[0]}.jpg`, `assets/images/${photos[1]}.jpg`]));
+  }
+  showMetamorphosis() {
+    this.popupShownMetamorphosis = true;
+  }
+  initMetamorphosis() {
+    this.metamorphosisElements.push(new GalleryElement(`York`, [`assets/images/metamorfozy/${16}.jpg`]));
+    this.metamorphosisElements.push(new GalleryElement(`Inny`, [`assets/images/metamorfozy/${17}.jpg`]));
+    this.metamorphosisElements.push(new GalleryElement(`Shitzu`, [`assets/images/metamorfozy/${18}.jpg`]));
+    this.metamorphosisElements.push(new GalleryElement(`West`, [`assets/images/metamorfozy/${19}.jpg`]));
+    this.metamorphosisElements.push(new GalleryElement(`Maltańczyk`, [`assets/images/metamorfozy/${20}.jpg`]));
+    this.metamorphosisElements.push(new GalleryElement(``, [`assets/images/metamorfozy/${21}.jpg`]));
+    this.metamorphosisElements.push(new GalleryElement(``, [`assets/images/metamorfozy/${22}.jpg`]));
+    this.metamorphosisElements.push(new GalleryElement(``, [`assets/images/metamorfozy/${72}.jpg`]));
+    this.metamorphosisElements.push(new GalleryElement(``, [`assets/images/metamorfozy/${88}.jpg`]));
+    this.metamorphosisElements.push(new GalleryElement(``, [`assets/images/metamorfozy/${89}.jpg`]));
+    this.metamorphosisElements.push(new GalleryElement(``, [`assets/images/metamorfozy/${90}.jpg`]));
+    this.metamorphosisElements.push(new GalleryElement(``, [`assets/images/metamorfozy/${91}.jpg`]));
+    this.metamorphosisElements.push(new GalleryElement(``, [`assets/images/metamorfozy/${92}.jpg`]));
+    this.metamorphosisElements.push(new GalleryElement(``, [`assets/images/metamorfozy/${93}.jpg`]));
   }
 }
